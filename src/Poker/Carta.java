360bbcee46bb3094d70053ba134756f919fd44b7
RSID99 <rolodelvalle15@gmail.com>
2024-11-27 09:39:59 -0300

    codigo modificado

--- src/Poker/Carta.java
+++ src/Poker/Carta.java
@@ -1,8 +1,16 @@
 package Poker;
 
+import java.util.Arrays;
+
 public class Carta {
     private String valor;
     private String palo;
+
+
+    public Carta(){
+        this.valor = "";
+        this.palo = "";
+    }
 
     public Carta(String valor, String palo){
         this.valor = valor;
@@ -27,6 +35,109 @@ public class Carta {
                 return 13;
             default:
                 return Integer.parseInt(valor);
+        }
+    }
+    public boolean color(Carta[] mano) {
+        String palo = mano[0].getPalo();
+        for (Carta carta : mano) {
+            if (carta.getPalo() != palo) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public boolean escalera(Carta[] mano) {
+        int[] valores = new int[mano.length];
+        for (int i = 0; i < mano.length; i++) {
+            valores[i] = mano[i].getValor();
+        }
+        Arrays.sort(valores);
+
+        for (int i = 0; i < valores.length - 1; i++) {
+            if (valores[i] + 1 != valores[i + 1]) {
+                return valores[0] == 2 && valores[1] == 3 && valores[2] == 4 &&
+                        valores[3] == 5 && valores[4] == 14;
+            }
+        }
+        return true;
+    }
+
+    public boolean doblePar (Carta[] mano) {
+        int pares = 0;
+        int [] frecuencias = contarFrecuencias(mano);
+        for (int frecuencia: frecuencias ){
+            if (frecuencia == 2){
+                pares ++;
+            }
+        }
+        return pares==2;
+    }
+
+    public boolean par (Carta[] mano) {
+        int [] frecuencias = contarFrecuencias(mano);
+        for (int frecuencia: frecuencias ){
+            if (frecuencia == 2){
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public boolean trio (Carta[] mano) {
+        int [] frecuencias = contarFrecuencias(mano);
+        for (int frecuencia: frecuencias ){
+            if (frecuencia == 3){
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public boolean full(Carta[] mano){
+        if (trio(mano) && par(mano)){
+            return true;
+        }
+        return false;
+    }
+
+    public boolean poker(Carta[] mano) {
+        int[] frecuencias = contarFrecuencias(mano);
+        for (int frecuencia : frecuencias) {
+            if (frecuencia == 4) {
+                return true;
+            }
+        }
+        return false;
+    }
+    //contar la cantidad de veces que aparece el valor
+    public int[] contarFrecuencias(Carta[] mano) {
+        int[] valores = new int[15];
+        for (Carta carta : mano) {
+            valores[carta.getValor()]++;
+        }
+        return valores;
+    }
+
+    public void identificarJugadas(Carta[] mano) {
+        if (color(mano) && escalera(mano)) {
+            System.out.println("Escalera Color");
+        } else if (poker(mano)) {
+            System.out.println("Poker");
+        } else if (full(mano)) {
+            System.out.println("Full");
+        } else if (color(mano)) {
+            System.out.println("Color");
+        } else if (escalera(mano)) {
+            System.out.println("Escalera");
+        } else if (trio(mano)) {
+            System.out.println("Trío");
+        } else if (doblePar(mano)) {
+            System.out.println("Doble Par");
+        } else if (par(mano)) {
+            System.out.println("Par");
+        } else {
+            System.out.println("Carta Alta");
         }
     }
 
--- src/Poker/Poker.java
+++ src/Poker/Poker.java
@@ -1,119 +1,58 @@
 package Poker;
-import java.util.Arrays;
+import java.util.Random;
 public class Poker {
     public static void main(String[] args) {
-        Poker poker = new Poker();
+        Random rnd = new Random();
+        String[] valores = new String[5];
+        String[] palos = new String[5];
+        for (int i = 0; i < valores.length; i++) {
+            int valor = rnd.nextInt(14) + 1;
+            valores[i] = String.valueOf(valor);
+        }
+
+        for (int i = 0; i < palos.length; i++) {
+            int palo = rnd.nextInt(4) + 1;
+            switch (palo) {
+                case 1:
+                    palos[i] = "S";
+                    break;
+                case 2:
+                    palos[i] = "C";
+                    break;
+                case 3:
+                    palos[i] = "H";
+                    break;
+                case 4:
+                    palos[i] = "D";
+                    break;
+                default:
+                    System.out.println("No Corresponde");
+                    break;
+            }
+        }
+
+        Carta[] mano = {
+                new Carta(valores[0], palos[0]),
+                new Carta(valores[1], palos[1]),
+                new Carta(valores[2], palos[2]),
+                new Carta(valores[3], palos[3]),
+                new Carta(valores[4], palos[4]),
+        };
+
+        /*//Probar de manera manual
         Carta[] mano = {
                 new Carta("4", "S"),
                 new Carta("5", "S"),
                 new Carta("6", "S"),
                 new Carta("7", "S"),
                 new Carta("8", "S")
-        };
+        };*/
+
+        Carta poker = new Carta();
         poker.identificarJugadas(mano);
-    }
 
-    public boolean color(Carta[] mano) {
-        String palo = mano[0].getPalo();
-        for (Carta carta : mano) {
-            if (carta.getPalo() != palo) {
-                return false;
-            }
-        }
-        return true;
-    }
 
-    public boolean escalera(Carta[] mano) {
-        int[] valores = new int[mano.length];
-        for (int i = 0; i < mano.length; i++) {
-            valores[i] = mano[i].getValor();
-        }
-        Arrays.sort(valores);
 
-        for (int i = 0; i < valores.length - 1; i++) {
-            if (valores[i] + 1 != valores[i + 1]) {
-                return valores[0] == 2 && valores[1] == 3 && valores[2] == 4 &&
-                        valores[3] == 5 && valores[4] == 14;
-            }
-        }
-        return true;
-    }
 
-    public boolean doblePar (Carta[] mano) {
-        int pares = 0;
-        int [] frecuencias = contarFrecuencias(mano);
-        for (int frecuencia: frecuencias ){
-            if (frecuencia == 2){
-                pares ++;
-            }
-        }
-        return pares==2;
-    }
-
-    public boolean par (Carta[] mano) {
-        int [] frecuencias = contarFrecuencias(mano);
-        for (int frecuencia: frecuencias ){
-            if (frecuencia == 2){
-                return true;
-            }
-        }
-        return false;
-    }
-
-    public boolean trio (Carta[] mano) {
-        int [] frecuencias = contarFrecuencias(mano);
-        for (int frecuencia: frecuencias ){
-            if (frecuencia == 3){
-                return true;
-            }
-        }
-        return false;
-    }
-
-    public boolean full(Carta[] mano){
-        if (trio(mano) && par(mano)){
-            return true;
-        }
-        return false;
-    }
-
-    public boolean poker(Carta[] mano) {
-        int[] frecuencias = contarFrecuencias(mano);
-        for (int frecuencia : frecuencias) {
-            if (frecuencia == 4) {
-                return true;
-            }
-        }
-        return false;
-    }
-    //contar la cantidad de veces que aparece el valor
-    public int[] contarFrecuencias(Carta[] mano) {
-        int[] valores = new int[15];
-        for (Carta carta : mano) {
-            valores[carta.getValor()]++;
-        }
-        return valores;
-    }
-
-    public void identificarJugadas(Carta[] mano) {
-        if (color(mano) && escalera(mano)) {
-            System.out.println("Escalera Color");
-        } else if (poker(mano)) {
-            System.out.println("Poker");
-        } else if (full(mano)) {
-            System.out.println("Full");
-        } else if (color(mano)) {
-            System.out.println("Color");
-        } else if (escalera(mano)) {
-            System.out.println("Escalera");
-        } else if (trio(mano)) {
-            System.out.println("Trío");
-        } else if (doblePar(mano)) {
-            System.out.println("Doble Par");
-        } else if (par(mano)) {
-            System.out.println("Par");
-        } else {
-            System.out.println("Carta Alta");
-        }
     }
 }
