abd1bd647c91800aa88de89391cd5a20bd8f0dae
RSID99 <rolodelvalle15@gmail.com>
2024-12-03 17:19:26 -0300

    Ejercicios JavaScript agregados

---
+++ JavaScript/Ejercitario1.js
@@ -0,0 +1,21 @@
+
+function ecuacionCuadratica(a,b,c){
+    if (a===0){
+        return "La ecuación no es de segundo grado";
+    }
+
+    let discriminante = b*b -4*a*c;
+
+    if (discriminante <0){
+       
+       return "No hay soluciones reales";
+    }
+
+    let raizCuadratrica = (-b+Math.sqrt(discriminante))/(2*a);
+
+
+    return raizCuadratrica;
+}
+
+let resultado = ecuacionCuadratica(8,2,1);
+console.log(resultado);
